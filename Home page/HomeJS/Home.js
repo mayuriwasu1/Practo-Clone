a61f4a10ccb5f39872eda62a6a3315162065f7dd
Atharva-s93 <athavaganbote@gmail.com>
2021-11-02 19:15:32 +0530

    Home page upadate

--- Home page/HomeJS/Home.js
+++ Home page/HomeJS/Home.js
@@ -1,6 +1,24 @@
-$('.autoplay').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-});
+
+
+// function menuClick(e){
+
+//   document.getElementById("forProvidersmanu").style.display = "block";
+
+// }
+
+// function DobesClick(){
+//   document.getElementById("forProvidersmanu").style.display = "none";
+// }
+
+
+document.querySelector("#Sendmes").addEventListener("click", Sendmes)
+
+function Sendmes() {
+  var MesNumber = document.getElementById("Numberformes").value;
+  var MesString = +MesNumber;
+  if(MesNumber.length == 10 && MesString > 0) {
+    document.getElementById("DisaplymesGrenn").style.display = "block";
+  } else{
+    document.getElementById("redborder").style.border = "thin solid red";
+  }
+}
