b698d42cd28bce8bf17d581327289b9df298f2df
Atharva-s93 <athavaganbote@gmail.com>
2021-11-06 20:07:38 +0530

    pull req

--- Home page/HomeJS/silder1.js
+++ Home page/HomeJS/silder1.js
@@ -8,8 +8,8 @@ $('.responsive').slick({
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 2,
+          slidesToScroll:1,
           infinite: true,
           dots: true
         }
@@ -17,8 +17,8 @@ $('.responsive').slick({
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToShow: 1,
+          slidesToScroll: 1
         }
       },
       {
@@ -41,3 +41,4 @@ $('.responsive').slick({
     autoplaySpeed: 1000,
     dots: true,
   });
+
