e84671f4da9c0bc59e947ab36c3ed1d6ae6c6435
Atharva-s93 <athavaganbote@gmail.com>
2021-11-11 01:59:10 +0530

    Home page resopive

--- Home page/HomeJS/Home.js
+++ Home page/HomeJS/Home.js
@@ -96,3 +96,36 @@ if (mediaQuery.matches) {
   document.querySelector(".heaing6 > h4").textContent = "Find doctors in top specialities";
   document.querySelector(".heaing6 > h4").style.fontSize = "medium";
 }
+
+
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+
+
+
+
+
+
+
+function showrespovemeni(){
+  document.querySelector(".respoviesNavbarleftmanu").classList.toggle('active')
+}
+
+function closethermeanu(){
+  document.querySelector(".respoviesNavbarleftmanu").classList.toggle('active')
+}
--- Home page/HomeJS/silder1.js
+++ Home page/HomeJS/silder1.js
@@ -8,7 +8,7 @@ $('.responsive').slick({
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 3,
           slidesToScroll:1,
           infinite: true,
           dots: true
@@ -17,7 +17,7 @@ $('.responsive').slick({
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 1,
+          slidesToShow: 3,
           slidesToScroll: 1
         }
       },
