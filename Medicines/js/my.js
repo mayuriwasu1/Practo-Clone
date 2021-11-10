2288ab8797e0ac7bb3b54f4472cba200e6eabae7
pavithran-paviii <pavithranr65@gmail.com>
2021-11-11 04:03:17 +0530

    Medicine page 90% completed

---
+++ Medicines/js/my.js
@@ -0,0 +1,18 @@
+$("#onePicSlide").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+
+$("#HCslider,#categories").slick({
+  infinite: false,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+});
+
+$("#popularProducts").slick({
+  infinite: false,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+});
