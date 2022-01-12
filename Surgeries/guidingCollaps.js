c98dc7e4ea71e2a3028011eb724211be13177f98
Atharva-s93 <athavaganbote@gmail.com>
2022-01-12 19:43:28 +0530

    update

--- Surgeries/guidingCollaps.js
+++ Surgeries/guidingCollaps.js
@@ -16,4 +16,32 @@ for (i = 0; i < coll.length; i++) {
 
 
 
-$('.single-item').slick();
+$('.single-item').slick({responsive: [
+  {
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll:1,
+      infinite: true,
+      dots: true
+    }
+  },
+  {
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 1
+    }
+  },
+  {
+    breakpoint: 499,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 1
+    }
+  }
+  // You can unslick at a given breakpoint now by adding:
+  // settings: "unslick"
+  // instead of a settings object
+]
+});
